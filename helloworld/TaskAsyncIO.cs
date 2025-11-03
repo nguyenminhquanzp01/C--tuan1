58f8ac5ee7a8d683a623bd563ddc077d93e8c968
quan <quan.nm225221@sis.hust.edu.vn>
2025-11-03 10:38:54 +0700

    first commit

---
+++ helloworld/TaskAsyncIO.cs
@@ -0,0 +1,38 @@
+using System.Threading.Tasks;
+
+public class TaskAsyncIO
+{
+  public static string path { get; set; } = "app.log";
+  public static async Task testAsync() //20s chậm hơn nhiều 
+  {
+    async Task readFile()
+    {
+      var text = await File.ReadAllTextAsync(path);
+      Console.WriteLine(text.Length);
+    }
+    List<Task> tasks = new();
+    for (int i = 0; i < 10; i++)
+    {
+      tasks.Add(readFile());
+    }
+    await Task.WhenAll(tasks);
+  }
+  public static void testNonAsync() // 10s
+  {
+    for (int i = 0; i < 10; i++)
+    {
+      var text = File.ReadAllText(path);
+      Console.WriteLine(text.Length);
+    }
+  }
+  public static async Task testAsync2()
+  {
+    var tasks = Enumerable.Range(0, 10)
+    .Select(async _ =>
+    {
+      var text = await File.ReadAllTextAsync(path);
+      Console.WriteLine(text.Length);
+    });
+    await Task.WhenAll(tasks);
+  }
+}
